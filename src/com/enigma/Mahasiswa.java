48dfc57e1c9d6827fbc5d644147e8da2ac706ffd
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-04-05 23:44:47 +0700

    java-collections

--- src/com/enigma/Mahasiswa.java
+++ src/com/enigma/Mahasiswa.java
@@ -1,2 +1,42 @@
-package com.enigma;public class Mahasiswa {
+package com.enigma;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Mahasiswa implements Serializable {
+    public static final long serialVersionUID =1L;
+
+    private String name;
+    private Integer age;
+    private  String major;
+
+    public Mahasiswa(String name, Integer age, String major) {
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
 }
--- src/com/enigma/java_file_io/Main.java
+++ src/com/enigma/java_file_io/Main.java
@@ -1,2 +1,91 @@
-package com.enigma.java_file_io;public class Main {
+package com.enigma.java_file_io;
+
+import com.enigma.Mahasiswa;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) {
+//       File file=new File("coba.txt");
+//        File file2=new File("coba2.txt");
+//        extracted(file);
+//        extracted1(file);
+//        extracted2(file);
+//        extracted3(file);
+        Path path=Path.of("mahasiswa");
+
+        try (OutputStream stream = Files.newOutputStream(path))
+        {
+            ObjectInputStream oos  = new ObjectInputStream(stream);
+            Mahasiswa mahasiswa=new Mahasiswa(
+                    "cahyo",17,"IT"
+            );
+            oos.writeObject(mahasiswa);
+            oos.close();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        //load
+        try(InputStream stream =Files.newInputStream(path)){
+            ObjectInputStream ois=new ObjectInputStream(stream);
+            Mahasiswa cahyo =(Mahasiswa) ois.readObject();
+            System.out.println(cahyo);
+            ois.close();
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void extracted3(File file) {
+        try (FileReader fd=new FileReader(file)){
+            while (true){
+                int ascii=fd.read();
+                if(ascii==-1)break;
+                System.out.println((char)ascii);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void extracted2(File file) {
+        try (FileWriter fw= new FileWriter(file)){
+            fw.write("Halo Enigma Camp");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void extracted1(File file) {
+        try (FileInputStream fis =new FileInputStream(file)){
+            while (true){
+                int ascii =fis.read();
+                if (ascii==-1)break;
+                System.out.println((char) ascii);
+
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void extracted(File file) {
+        try {
+            FileOutputStream fos = new FileOutputStream(file, true);
+            fos.write(65);
+            fos.write(72);
+            fos.write(77);
+            fos.write(65);
+            fos.write(68);
+            fos.write(" Tanto Wiraga".getBytes(StandardCharsets.UTF_8));
+            fos.close();
+        }catch (IOException exception){
+                exception.printStackTrace();
+            }
+    }
 }
